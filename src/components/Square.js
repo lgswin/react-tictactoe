2ab44b540080c9902d737d6bcdb9518de43962d6
lgswin <lgswin@gmail.com>
2023-05-11 21:48:51 +0900

    react : constructor, ...

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,18 +3,11 @@ import "./Square.css"
 
 export default class Square extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: null
-        }
-    }
-
     render() {
         return (
             <button className='square' 
-                onClick={()=>{ this.setState({value: 'X'})}}>
-                {this.state.value}
+                onClick={()=>{ this.props.onClick()}}>
+                {this.props.value}
             </button>
         )
     }
